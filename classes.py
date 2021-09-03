7ca2366b16d65a4743c8082470ccd8acd6010a5c
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-09-03 18:22:35 +0300

    File format

---
+++ classes.py
@@ -0,0 +1,54 @@
+class SakClass:
+    # letters
+
+    def __init__(self) -> None:
+        pass
+
+    def randomize_sak():
+        pass
+
+    def getletters():
+        pass
+
+    def putbackletters():
+        pass
+
+class Player:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+class Human(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def play():
+        pass
+
+class Computer(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def play():
+        pass
+
+class Game:
+    score = 0
+    sak = SakClass
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def setup():
+        pass
+
+    def run():
+        pass
+
+    def end():
+        pass
+
